b4ff59da4bcd9ed25c58fc6828e5afbcc9f03c91
David Betz <david.betz@st.com>
2016-05-05 16:44:22 -0400

    Add a release script.

---
+++ release/flash.sh
@@ -0,0 +1,30 @@
+PORT=/dev/ttyUSB0
+if [ ! -z $1 ]
+then
+  PORT=$1
+fi
+echo Using port $PORT
+
+BAUD=115200
+BOARD=none # none, ck, nodemcu, wifio
+FLASH_SIZE=4M
+FLASH_BLOCK_SIZE=1024
+FLASH_SPEED=80
+FLASH_INTERFACE=qio
+
+#use for 1MB flash
+#WIFI_SETTINGS=0xFE000
+
+#use for 4MB flash
+WIFI_SETTINGS=0x3FE000
+
+esptool \
+-cp $PORT \
+-cd $BOARD \
+-cb $BAUD \
+-bz $FLASH_SIZE \
+-bf $FLASH_SPEED \
+-bm $FLASH_INTERFACE \
+-ca 0x00000 -cf 0x00000.bin \
+-ca 0x40000 -cf 0x40000.bin \
+-ca $WIFI_SETTINGS -cf blank.bin
